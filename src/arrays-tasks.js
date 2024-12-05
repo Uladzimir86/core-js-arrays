fd7ca0d42bf7ec5eaa1b7f209c03f918f904f8ec
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2024-12-05 23:20:17 +0300

    solve sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,16 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let maxLength = Math.max(arr1.length);
+  let maxArr = arr1;
+  let minArr = arr2;
+  if (maxLength < arr2.length) {
+    maxLength = arr2.length;
+    maxArr = arr2;
+    minArr = arr1;
+  }
+  return maxArr.map((el, ind) => el + (minArr[ind] || 0));
 }
 
 /**
